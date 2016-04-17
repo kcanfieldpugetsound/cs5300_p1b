1901100c230a3acc59e2522d1a0eccef7bf97c7e
kcanfieldpugetsound <kcanfield@mac.com>
2016-04-17 17:46:59 -0400

    adds search methods and fills in sessionSearch()

--- eclipse/src/edu/cornell/cs5300/servers/rpc/Client.java
+++ eclipse/src/edu/cornell/cs5300/servers/rpc/Client.java
@@ -5,6 +5,12 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import edu.cornell.cs5300.project1b.IPAddress;
+import edu.cornell.cs5300.project1b.rpc.message.RPCMessage;
+import edu.cornell.cs5300.project1b.rpc.message.RPCMessageInterpreter;
+import edu.cornell.cs5300.project1b.servlet.session.Session;
+import edu.cornell.cs5300.project1b.servlet.session.SessionId;
+
 public class Client {
 	
 	public static ConcurrentLinkedQueue<DatagramPacket> push_responses = new ConcurrentLinkedQueue<DatagramPacket>();
@@ -23,9 +29,7 @@ public class Client {
 		new ClientThread().start();
 	}
 	
-	
-	
-	public boolean sendPacket(DatagramPacket packet)
+	public static boolean sendPacket(DatagramPacket packet)
 	{
 		try {
 			Client.clientSocket.send(packet);
@@ -35,6 +39,37 @@ public class Client {
 			e.printStackTrace();
 			return false;
 		}
+		return true;
+	}
+	
+	//search data responses
+	public Session sessionSearch(SessionId id, IPAddress addr)
+	{
+		Object[] responses= data_responses.toArray();
+		for(int i=0; i<data_responses.size(); i++)
+		{
+			RPCMessage m = new RPCMessage(((DatagramPacket)responses[i]).getData());
+			RPCMessageInterpreter interpreter = new RPCMessageInterpreter(m);
+			SessionId responseSessionID = interpreter.sessionId();
+			
+			if(id.toStringWithoutVersion().equals(responseSessionID))
+			{
+				return new Session(id,interpreter.userData());
+			}
+		}
+		return null;
+	}
+	
+	
+	//search push responses
+	public boolean getACK(SessionId id, IPAddress addr)
+	{
+		Object[] acks= push_responses.toArray();
+		for(int j=0; j<acks.length; j++)
+		{
+			
+		}
+		
 		return true;
 	}
 	
